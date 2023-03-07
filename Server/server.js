b6fcba1e531597fba4c1bbcd835b378208edbec6
chigoox <dikeemmanyel54@gmail.com>
2023-03-06 23:13:15 -0500

    updated env server

--- Server/server.js
+++ Server/server.js
@@ -4,12 +4,16 @@ const sgMail = require('@sendgrid/mail')
 require('dotenv').config({ path: './.env' })
 
 
+
+
+
+
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
-sgMail.setApiKey('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') //(process.env.SENDGRID_API_KEY)
+sgMail.setApiKey (process.env.SENDGRID_API_KEY)//('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') //
 
-const stripe = require('stripe')('sk_test_51MamPiLPNdUzkCF3xdRvn0nkLpOrsJFo1um4Z7e07FlQXH6T7HCHhRxYkVjkK2iPW61EMZKoDM0ml6YSdWmAPcEn00E3jb1Gcr', {
+const stripe = require('stripe')(STRIPE_SECRET_API_KEY/* 'sk_test_51MamPiLPNdUzkCF3xdRvn0nkLpOrsJFo1um4Z7e07FlQXH6T7HCHhRxYkVjkK2iPW61EMZKoDM0ml6YSdWmAPcEn00E3jb1Gcr' */, {
   apiVersion: '2020-08-27',
   appInfo: { // For sample support and debugging, not required for production:
     name: "stripe-samples/checkout-one-time-payments",
@@ -20,8 +24,8 @@ const stripe = require('stripe')('sk_test_51MamPiLPNdUzkCF3xdRvn0nkLpOrsJFo1um4Z
 
 
 function SendSMS(number, txt){
-  const accountSid = 'ACbe9bf767831def2a48f2248159f21063'//process.env.TWILIO_ACCOUNT_SID;
-  const authToken = '282e78a16c0df0f1cf73acbf4bdab09d'//process.env.TWILIO_AUTH_TOKEN;
+  const accountSid = process.env.TWILIO_ACCOUNT_SID //'ACbe9bf767831def2a48f2248159f21063'//;
+  const authToken = process.env.TWILIO_AUTH_TOKEN  //'282e78a16c0df0f1cf73acbf4bdab09d'//;
   const client = require('twilio')(accountSid, authToken);
 client.messages
   .create({
